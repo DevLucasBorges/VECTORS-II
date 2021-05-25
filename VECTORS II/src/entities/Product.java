1892fdce961e9e71fefbb7d85727b833a9b7f1f1
Lucas Borges <76432049+DevLucasBorges@users.noreply.github.com>
2021-05-25 19:31:09 -0300

    Add files via upload

---
+++ VECTORS II/src/entities/Product.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Product {
+
+	public Product(String name, double price) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public double getPrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
